4463b892ce3c52ff7be87da2e453aa9350a320da
Hyenath <nathan.dubaille@gmail.com>
2025-05-02 11:57:10 +0200

    check

--- back/Middlewares/check-token.js
+++ back/Middlewares/check-token.js
@@ -6,18 +6,16 @@ const config = require('../config.json');
 function checkToken(req, res, next) {
     const token = req.headers['authorization'];
 
-    if (!token || !token.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Token manquant ou mal formaté' });
-    }
-
-    jwt.verify(token.split(' ')[1], config.key, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Token invalide ou expiré' });
-        }
-
-        req.user = decoded;
+    if (token) {
+        jwt.verify(token.split(' ')[1], config.key, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: 'Token invalide ou expiré' });
+            }
+            return res.status(400).json({ message: 'Déjà connecté' });
+        });
+    } else {
         next();
-    });
+    }
 }
 
 module.exports = checkToken;
